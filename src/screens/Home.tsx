5a35b497d5217b5d5798dc1f8c9798e7f29da5b7
devGbarbosa <barbosagdev@gmail.com>
2022-04-22 17:58:07 -0300

    feat: modify flexbox from home screen

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,6 +2,7 @@ import React, { FC, useEffect } from 'react'
 import { FlatList, SafeAreaView, Text, View } from 'react-native'
 import { useAppDispatch, useAppSelector } from '../hooks'
 import { saveProducts } from '../store/actions/products'
+import { ProductState } from '../store/reducers/products'
 
 export const Home: FC = () => {
   const dispatch = useAppDispatch()
@@ -10,14 +11,21 @@ export const Home: FC = () => {
     dispatch(saveProducts(['Bag', 'Pencil', 'Eraser', 'Ball']))
   }, [])
 
-  const products = useAppSelector(state => state.products.products)
+  const { products } = useAppSelector<ProductState>((state) => state.products)
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <View style={{ flex: 1, alignItems: 'center' }}>
-        <Text>Hello world!</Text>
-
+      <View
+        style={{
+          flex: 1,
+          alignItems: 'center',
+        }}
+      >
+        <View style={{ marginTop: 16 }}>
+          <Text style={{fontSize: 24  }}>List of products</Text>
+        </View>
         <FlatList
+          contentContainerStyle={{ flexGrow: 1, justifyContent: 'center' }}
           data={products}
           renderItem={({ item }) => (
             <View>
